4e21084186c051e434b355568d1e580ed90d8b8d
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-12 10:14:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from image_processing import app1 as image_app1, app2 as image_app2
-from table_processing import app1 as table_app1, app2 as table_app2
-from data_analysis import app1 as data_app1, app2 as data_app2
-from utility_tools import app1 as utility_app1, app2 as utility_app2
+from image_processing import image_app1, image_app2
+from table_processing import table_app1, table_app2
+from data_analysis import data_app1, data_app2
+from utility_tools import utility_app1, utility_app2
 
 def main():
     st.title("多个小程序展示页面")
@@ -13,31 +13,32 @@ def main():
     # 根据用户选择展示不同分类下的小程序内容
     if selected_category == "图片处理":
         st.subheader("图片处理小程序1")
-        image_app1()
+        image_app1.run()
 
         st.subheader("图片处理小程序2")
-        image_app2()
+        image_app2.run()
 
     elif selected_category == "表格处理":
         st.subheader("表格处理小程序1")
-        table_app1()
+        table_app1.run()
 
         st.subheader("表格处理小程序2")
-        table_app2()
+        table_app2.run()
 
     elif selected_category == "数据分析":
         st.subheader("数据分析小程序1")
-        data_app1()
+        data_app1.run()
 
         st.subheader("数据分析小程序2")
-        data_app2()
+        data_app2.run()
 
     elif selected_category == "实用工具":
         st.subheader("实用工具小程序1")
-        utility_app1()
+        utility_app1.run()
 
         st.subheader("实用工具小程序2")
-        utility_app2()
+        utility_app2.run()
 
 if __name__ == "__main__":
     main()
+
