8ca0d7e642dab2511870d03aa77868d6abf9e0a8
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-12 09:18:47 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from 16宫格图片 import 16宫格图片
+from 9宫格图片 import 9宫格图片
+
+def main():
+    st.title("多个小程序展示页面")
+
+    # 在侧边栏展示小程序选项
+    app_choice = st.sidebar.selectbox("选择要展示的小程序", ["9宫格图片", "16宫格图片"])
+
+    # 根据用户选择展示不同的小程序内容
+    if app_choice == "9宫格图片":
+        app1()
+    elif app_choice == "16宫格图片":
+        app2()
+
+if __name__ == "__main__":
+    main()
