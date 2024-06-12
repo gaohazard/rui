c9ef23c2d55ddf2b7def540aed18a7ba4231d47e
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-12 14:16:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from image_app1 import app1 as image_app1
 from image_app2 import app2 as image_app2
 #from image_app3 import app3 as image_app3
-import cv2
+#import cv2
 #from table_app1 import app1 as table_app1
 #from table_app2 import app2 as table_app2
 #from data_app1 import app1 as data_app1
@@ -50,9 +50,9 @@ def main():
             #utility_app1()
         #elif utility_type == "实用工具2":
             #utility_app2()
-    #elif process_type == "工作使用":
-        #st.sidebar.subheader("选择工作使用类型")
-        #work_type = st.sidebar.selectbox("选择工作使用类型", ["工作使用1", "工作使用2"])
+    elif process_type == "工作使用":
+        st.sidebar.subheader("选择工作使用类型")
+        work_type = st.sidebar.selectbox("选择工作使用类型", ["工作使用1", "工作使用2"])
 
         if work_type == "工作使用1":
             work_app1()
