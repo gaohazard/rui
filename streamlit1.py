405c1df177d9072cb870f57f056af813d5f4abeb
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-12 09:00:17 +0800

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageChops, ImageOps, ImageSequence
 import os
 import base64
 
+# 设置页面布局
+st.set_page_config(layout="wide", page_title="a 16-grid image into a GIF animation", page_icon="📊")
+
+
 def trim(image):
     bg = Image.new(image.mode, image.size, image.getpixel((0,0)))
     diff = ImageChops.difference(image, bg)
