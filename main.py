f40bfeefbd2bd27338380bd14fbbe728388d37cf
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-12 14:15:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from image_app1 import app1 as image_app1
 from image_app2 import app2 as image_app2
-from image_app3 import app3 as image_app3
+#from image_app3 import app3 as image_app3
 import cv2
 #from table_app1 import app1 as table_app1
 #from table_app2 import app2 as table_app2
@@ -9,7 +9,7 @@ import cv2
 #from data_app2 import app2 as data_app2
 #from utility_app1 import app1 as utility_app1
 #from utility_app2 import app2 as utility_app2
-#from work_app1 import app1 as work_app1
+from work_app1 import app1 as work_app1
 #from work_app2 import app2 as work_app2
 
 def main():
@@ -24,8 +24,8 @@ def main():
             image_app1()
         elif image_type == "16宫格图片":
             image_app2()
-        elif image_type == "修改照片格式":
-            image_app3()
+        #elif image_type == "修改照片格式":
+            #image_app3()
     #elif process_type == "表格处理":
         #st.sidebar.subheader("选择表格类型")
         #table_type = st.sidebar.selectbox("选择表格类型", ["表格处理1", "表格处理2"])
@@ -54,8 +54,8 @@ def main():
         #st.sidebar.subheader("选择工作使用类型")
         #work_type = st.sidebar.selectbox("选择工作使用类型", ["工作使用1", "工作使用2"])
 
-        #if work_type == "工作使用1":
-            #work_app1()
+        if work_type == "工作使用1":
+            work_app1()
         #elif work_type == "工作使用2":
             #work_app2()
 if __name__ == "__main__":
