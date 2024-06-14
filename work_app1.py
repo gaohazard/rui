80018ac6b98b185ac57446ad17fac6b22264aa22
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-14 09:51:58 +0800

    Update work_app1.py

--- work_app1.py
+++ work_app1.py
@@ -2,11 +2,8 @@ import streamlit as st
 import numpy as np
 from scipy.interpolate import interp1d, interp2d
 
-
-def calculate_weight_balance(aircraft_model, fuel_density, empty_weight, cg_position, left_side_fuel_volume_new,center_side_fuel_volume_new, crew_weight, add_weight, additional_moment):
-    
+def calculate_weight_balance(aircraft_model, fuel_density, empty_weight, cg_position, left_side_fuel_volume_new, center_side_fuel_volume_new, crew_weight, add_weight, additional_moment):
     if aircraft_model == '737-800':
-        # 定义插值所需的数据
         left_side_fuel_volume = np.array(
             [0, 400, 800, 1200, 1600, 2000, 2400, 2800, 3200, 3600, 4000, 4400, 4800, 5200, 5600, 6000, 6400, 6800,
              7200, 7600, 8000, 8400, 8800, 9200, 9600, 9751])
@@ -59,35 +56,30 @@ def calculate_weight_balance(aircraft_model, fuel_density, empty_weight, cg_posi
     else:
         print("机型输入错误")
         return None, None
-    # 定义插值函数
+
     left_interp_func = interp1d(left_side_fuel_volume, additional_lever_arm_left, kind='linear')
     center_interp_func = interp1d(center_fuel_volume, additional_lever_arm_center, kind='linear')
 
-    # 使用插值函数计算左、右、中三个位置的附加力臂
     new_additional_lever_arm_left = left_interp_func(left_side_fuel_volume_new)
-    new_additional_lever_arm_center = center_interp_func(center_side_fuel_volume_new )
+    new_additional_lever_arm_center = center_interp_func(center_side_fuel_volume_new)
 
-    # 计算总燃料量、总重量、总力矩和总力臂
     lever_arm = cg_position * 155.8 / 100 + 627.1
     moment = lever_arm * empty_weight
-    total_weight = empty_weight +left_side_fuel_volume_new*fuel_density + center_side_fuel_volume_new*fuel_density + crew_weight + add_weight
+    total_weight = empty_weight + left_side_fuel_volume_new * fuel_density + center_side_fuel_volume_new * fuel_density + crew_weight + add_weight
 
-    total_moment = (moment+left_side_fuel_volume_new * fuel_density * new_additional_lever_arm_left +
-                    center_side_fuel_volume_new * fuel_density * new_additional_lever_arm_center+additional_moment+crew_weight_moment)
+    total_moment = (moment + left_side_fuel_volume_new * fuel_density * new_additional_lever_arm_left +
+                    center_side_fuel_volume_new * fuel_density * new_additional_lever_arm_center + additional_moment + crew_weight_moment)
     total_arm = total_moment / total_weight
 
-    # 计算新的重心位置
     cg_position_new = (total_arm - 627.1) * 100 / 155.8
 
     return total_weight, cg_position_new
 
 def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
     if aircraft_model == '737-800':
-
         cg = np.array([5, 10, 15, 20, 25, 30, 35])
         gw = np.array([80, 100, 120, 140, 160, 180, 200])
 
-    # 定义不同磨擦系数对应的二维数据
         mw_no_brake_coefficient_1 = np.array([[17, 16, 15, 14, 13, 11, 10],
                                               [20, 18, 17, 16, 14, 13, 11],
                                               [21, 20, 19, 17, 16, 14, 12],
@@ -216,11 +208,9 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
                                                 [81, 82, 79, 78, 72, 64, 54],
                                                 [86, 86, 84, 82, 76, 67, 57]])
     elif aircraft_model == '737-700':
-
         cg = np.array([5, 10, 15, 20, 25, 30, 35])
         gw = np.array([80, 100, 120, 140, 160, 180, 200])
 
-    # 定义不同磨擦系数对应的二维数据
         mw_no_brake_coefficient_1 = np.array([[17, 16, 15, 14, 13, 11, 10],
                                               [20, 18, 17, 16, 14, 13, 11],
                                               [21, 20, 19, 17, 16, 14, 12],
@@ -336,10 +326,10 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
         mw_with_brake_coefficient_7 = np.array([[52, 52, 51, 50, 48, 43, 37],
                                                 [59, 59, 57, 56, 54, 48, 41],
                                                 [65, 65, 63, 61, 59, 52, 44],
-                                                [71, 70, 68, 66, 63, 56, 48],
-                                                [76, 75, 73, 71, 68, 60, 51],
-                                                [81, 79, 77, 75, 72, 64, 54],
-                                                [86, 84, 82, 80, 76, 67, 57]])
+                                                [71, 71, 68, 66, 63, 56, 48],
+                                                [76, 76, 73, 71, 68, 60, 51],
+                                                [81, 81, 77, 75, 72, 64, 54],
+                                                [86, 86, 82, 80, 76, 67, 57]])
 
         mw_with_brake_coefficient_8 = np.array([[52, 53, 52, 51, 48, 43, 37],
                                                 [59, 60, 59, 57, 54, 48, 41],
@@ -369,12 +359,12 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
                                               [38, 36, 33, 31, 28, 25, 21]])
 
         mw_no_brake_coefficient_3 = np.array([[28, 27, 25, 23, 21, 18, 16],
-                                            [32, 30, 28, 26, 23, 21, 18],
-                                            [35, 33, 31, 28, 26, 23, 19],
-                                            [38, 35, 33, 31, 28, 25, 21],
-                                            [40, 38, 35, 33, 30, 26, 22],
-                                            [43, 40, 38, 35, 32, 28, 24],
-                                            [45, 42, 40, 37, 33, 30, 25]])
+                                              [32, 30, 28, 26, 23, 21, 18],
+                                              [35, 33, 31, 28, 26, 23, 19],
+                                              [38, 35, 33, 31, 28, 25, 21],
+                                              [40, 38, 35, 33, 30, 26, 22],
+                                              [43, 40, 38, 35, 32, 28, 24],
+                                              [45, 42, 40, 37, 33, 30, 25]])
 
         mw_no_brake_coefficient_4 = np.array([[32, 30, 28, 26, 24, 21, 18],
                                               [36, 34, 31, 29, 26, 23, 20],
@@ -393,28 +383,28 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
                                               [55, 52, 49, 45, 41, 36, 31]])
 
         mw_no_brake_coefficient_6 = np.array([[37, 35, 33, 30, 27, 24, 20],
-                                            [42, 39, 37, 34, 31, 27, 23],
-                                            [46, 43, 40, 37, 34, 30, 25],
-                                            [50, 47, 44, 40, 37, 32, 28],
-                                            [53, 50, 47, 43, 39, 35, 30],
-                                            [56, 53, 49, 46, 41, 37, 31],
-                                            [59, 56, 52, 48, 44, 39, 33]])
+                                              [42, 39, 37, 34, 31, 27, 23],
+                                              [46, 43, 40, 37, 34, 30, 25],
+                                              [50, 47, 44, 40, 37, 32, 28],
+                                              [53, 50, 47, 43, 39, 35, 30],
+                                              [56, 53, 49, 46, 41, 37, 31],
+                                              [59, 56, 52, 48, 44, 39, 33]])
 
         mw_no_brake_coefficient_7 = np.array([[40, 37, 35, 32, 29, 26, 22],
-                                            [44, 42, 39, 36, 33, 29, 24],
-                                            [49, 46, 43, 39, 36, 32, 27],
-                                            [52, 49, 46, 43, 39, 34, 29],
-                                            [56, 53, 49, 45, 41, 37, 31],
-                                            [59, 56, 52, 48, 44, 39, 33],
-                                            [63, 59, 55, 51, 46, 41, 35]])
+                                              [44, 42, 39, 36, 33, 29, 24],
+                                              [49, 46, 43, 39, 36, 32, 27],
+                                              [52, 49, 46, 43, 39, 34, 29],
+                                              [56, 53, 49, 45, 41, 37, 31],
+                                              [59, 56, 52, 48, 44, 39, 33],
+                                              [63, 59, 55, 51, 46, 41, 35]])
 
         mw_no_brake_coefficient_8 = np.array([[41, 39, 36, 34, 30, 27, 23],
-                                            [46, 44, 41, 38, 34, 30, 25],
-                                            [51, 48, 45, 41, 37, 33, 28],
-                                            [55, 52, 48, 44, 40, 36, 30],
-                                            [59, 55, 52, 48, 43, 38, 33],
-                                            [62, 59, 55, 50, 46, 41, 35],
-                                            [66, 62, 58, 53, 48, 43, 37]])
+                                              [46, 44, 41, 38, 34, 30, 25],
+                                              [51, 48, 45, 41, 37, 33, 28],
+                                              [55, 52, 48, 44, 40, 36, 30],
+                                              [59, 55, 52, 48, 43, 38, 33],
+                                              [62, 59, 55, 50, 46, 41, 35],
+                                              [66, 62, 58, 53, 48, 43, 37]])
 
         mw_with_brake_coefficient_1 = np.array([[26, 25, 24, 24, 23, 22, 22],
                                                 [30, 28, 27, 26, 26, 25, 24],
@@ -481,6 +471,8 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
                                                 [85, 85, 83, 81, 74, 65, 55]])
     else:
         print('机型输入错误')
+        return None
+
     if brake:
         if friction_coefficient == 0.1:
             mw = mw_with_brake_coefficient_1
@@ -500,6 +492,7 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
             mw = mw_with_brake_coefficient_8
         else:
             print("摩擦系数输入错误")
+            return None
     else:
         if friction_coefficient == 0.1:
             mw = mw_no_brake_coefficient_1
@@ -519,6 +512,7 @@ def estimate_ba(cg_new, gw_new, brake, friction_coefficient, aircraft_model):
             mw = mw_no_brake_coefficient_8
         else:
             print("摩擦系数输入错误")
+            return None
 
     interpolation_function = interp2d(cg, gw, mw, kind='linear', fill_value='extrapolate')
     return interpolation_function(cg_new, gw_new)
@@ -535,7 +529,7 @@ class EstimateBaApp:
         self.crew_weight = st.number_input('试车人员总重量(kg)（如试车）')
         self.add_weight = st.number_input('其他需要加减重量(lb)')
         self.additional_moment = st.number_input('其他需要加减重量对应的力矩值(lb.in)')
-        self.brake = st.number_input('是否设置停留刹车（是/否）')
+        self.brake = st.radio('是否设置停留刹车', ('是', '否'))
         self.friction_coefficient = st.number_input('请输入摩擦系数（0.1-0.8）')
 
         self.calculate_button = st.button('计算')
@@ -544,72 +538,88 @@ class EstimateBaApp:
 
     def calculate(self):
         if self.aircraft_model == '737-800':
-            fuel_density = float(self.fuel_density* 2.20462262185)
-
-            index=float(self.index)
-            gw_new = float(self.gw_new)
-            left_side_fuel_volume_new = float(self.left_side_fuel_volume_new) / fuel_density
-            center_side_fuel_volume_new = float(self.center_side_fuel_volume_new) / fuel_density  # 根据需要设置默认值
-            crew_weight = float(self.crew_weight) * 2.20462262185  # 根据需要设置默认值
-            add_weight = float(self.add_weight)  # 根据需要设置默认值
-            additional_moment = float(self.additional_moment)  # 根据需要设置默认值
-            brake = str(self.brake)
-            cg_new=((index-45)*77162/gw_new+658.3-627.1)/1.558
-            friction_coefficient = float(self.friction_coefficient)
-            total_weight, cg_position_new = calculate_weight_balance(self.aircraft_model, fuel_density, gw_new,
-                                                                     cg_new, left_side_fuel_volume_new,
-                                                                     center_side_fuel_volume_new, crew_weight,
-                                                                     add_weight, additional_moment)
-            print(gw_new,cg_new,total_weight, cg_position_new)
-            mw_new = estimate_ba(cg_position_new, total_weight/1000, brake, friction_coefficient,self.aircraft_model)
-            result_text = f"最大抗风值(节)：{mw_new}\n飞机重量（lb）：{total_weight}\n飞机重心位置（%）：{cg_position_new}"
-            st.write(result_text)
-
-        elif self.aircraft_model =='737-8':
             fuel_density = float(self.fuel_density) * 2.20462262185
-
             index = float(self.index)
             gw_new = float(self.gw_new)
             left_side_fuel_volume_new = float(self.left_side_fuel_volume_new) / fuel_density
-            center_side_fuel_volume_new = float(
-                self.center_side_fuel_volume_new) / fuel_density  # 根据需要设置默认值
-            crew_weight = float(self.crew_weight) * 2.20462262185  # 根据需要设置默认值
-            add_weight = float(self.add_weight)  # 根据需要设置默认值
-            additional_moment = float(self.additional_moment)  # 根据需要设置默认值
-            brake = str(self.brake)
+            center_side_fuel_volume_new = float(self.center_side_fuel_volume_new) / fuel_density
+            crew_weight = float(self.crew_weight) * 2.20462262185
+            add_weight = float(self.add_weight)
+            additional_moment = float(self.additional_moment)
+            brake = self.brake == '是'
             cg_new = ((index - 45) * 77162 / gw_new + 658.3 - 627.1) / 1.558
             friction_coefficient = float(self.friction_coefficient)
             total_weight, cg_position_new = calculate_weight_balance(self.aircraft_model, fuel_density, gw_new,
                                                                      cg_new, left_side_fuel_volume_new,
                                                                      center_side_fuel_volume_new, crew_weight,
                                                                      add_weight, additional_moment)
-            print(gw_new, cg_new, total_weight, cg_position_new)
-            mw_new = estimate_ba(cg_position_new, total_weight/1000, brake, friction_coefficient, self.aircraft_model)
+            if total_weight is None or cg_position_new is None:
+                st.write("计算过程中出现错误")
+                return
+
+            mw_new = estimate_ba(cg_position_new, total_weight / 1000, brake, friction_coefficient, self.aircraft_model)
+            if mw_new is None:
+                st.write("计算过程中出现错误")
+                return
+
             result_text = f"最大抗风值(节)：{mw_new}\n飞机重量（lb）：{total_weight}\n飞机重心位置（%）：{cg_position_new}"
             st.write(result_text)
-        elif self.aircraft_model == '737-700':
-            fuel_density = float(self.fuel_density) * 2.20462262185
 
+        elif self.aircraft_model == '737-8':
+            fuel_density = float(self.fuel_density) * 2.20462262185
             index = float(self.index)
             gw_new = float(self.gw_new)
             left_side_fuel_volume_new = float(self.left_side_fuel_volume_new) / fuel_density
-            center_side_fuel_volume_new = float(
-                self.center_side_fuel_volume_new) / fuel_density  # 根据需要设置默认值
-            crew_weight = float(self.crew_weight) * 2.20462262185  # 根据需要设置默认值
-            add_weight = float(self.add_weight)  # 根据需要设置默认值
-            additional_moment = float(self.additional_moment)  # 根据需要设置默认值
-            brake = str(self.brake)
+            center_side_fuel_volume_new = float(self.center_side_fuel_volume_new) / fuel_density
+            crew_weight = float(self.crew_weight) * 2.20462262185
+            add_weight = float(self.add_weight)
+            additional_moment = float(self.additional_moment)
+            brake = self.brake == '是'
+            cg_new = ((index - 45) * 77162 / gw_new + 658.3 - 627.1) / 1.558
+            friction_coefficient = float(self.friction_coefficient)
+            total_weight, cg_position_new = calculate_weight_balance(self.aircraft_model, fuel_density, gw_new,
+                                                                     cg_new, left_side_fuel_volume_new,
+                                                                     center_side_fuel_volume_new, crew_weight,
+                                                                     add_weight, additional_moment)
+            if total_weight is None or cg_position_new is None:
+                st.write("计算过程中出现错误")
+                return
+
+            mw_new = estimate_ba(cg_position_new, total_weight / 1000, brake, friction_coefficient, self.aircraft_model)
+            if mw_new is None:
+                st.write("计算过程中出现错误")
+                return
+
+            result_text = f"最大抗风值(节)：{mw_new}\n飞机重量（lb）：{total_weight}\n飞机重心位置（%）：{cg_position_new}"
+            st.write(result_text)
+
+        elif self.aircraft_model == '737-700':
+            fuel_density = float(self.fuel_density) * 2.20462262185
+            index = float(self.index)
+            gw_new = float(self.gw_new)
+            left_side_fuel_volume_new = float(self.left_side_fuel_volume_new) / fuel_density
+            center_side_fuel_volume_new = float(self.center_side_fuel_volume_new) / fuel_density
+            crew_weight = float(self.crew_weight) * 2.20462262185
+            add_weight = float(self.add_weight)
+            additional_moment = float(self.additional_moment)
+            brake = self.brake == '是'
             cg_new = ((index - 45) * 66139 / gw_new + 658.3 - 627.1) / 1.558
             friction_coefficient = float(self.friction_coefficient)
             total_weight, cg_position_new = calculate_weight_balance(self.aircraft_model, fuel_density, gw_new,
                                                                      cg_new, left_side_fuel_volume_new,
                                                                      center_side_fuel_volume_new, crew_weight,
                                                                      add_weight, additional_moment)
-            print(gw_new, cg_new, total_weight, cg_position_new)
-            mw_new = estimate_ba(cg_position_new, total_weight/1000, brake, friction_coefficient, self.aircraft_model)
+            if total_weight is None or cg_position_new is None:
+                st.write("计算过程中出现错误")
+                return
+
+            mw_new = estimate_ba(cg_position_new, total_weight / 1000, brake, friction_coefficient, self.aircraft_model)
+            if mw_new is None:
+                st.write("计算过程中出现错误")
+                return
+
             result_text = f"最大抗风值(节)：{mw_new}\n飞机重量（lb）：{total_weight}\n飞机重心位置（%）：{cg_position_new}"
             st.write(result_text)
 
-# 创建Streamlit应用
-app1 = EstimateBaApp()
-
+if __name__ == '__main__':
+    EstimateBaApp()
