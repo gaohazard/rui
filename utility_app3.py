67ccee58b3bd407c6ab3b2607044d9ec6415854f
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-21 14:34:21 +0800

    Update utility_app3.py

--- utility_app3.py
+++ utility_app3.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import speech_recognition as sr
-import audioread
+from mutagen.mp3 import MP3
 import wave
-import os
+import numpy as np
+from io import BytesIO
 
 def transcribe_audio(audio_file):
     recognizer = sr.Recognizer()
@@ -11,18 +12,19 @@ def transcribe_audio(audio_file):
         text = recognizer.recognize_google(audio_data)
     return text
 
-def convert_to_wav(mp3_path):
-    wav_path = "temp_audio_file.wav"
-    with audioread.audio_open(mp3_path) as input_file:
-        with wave.open(wav_path, "wb") as output_file:
-            output_file.setnchannels(input_file.channels)
-            output_file.setsampwidth(2)  # 16-bit samples
-            output_file.setframerate(input_file.samplerate)
+def convert_to_wav(mp3_bytes):
+    mp3_audio = MP3(BytesIO(mp3_bytes))
+    audio_data = mp3_audio.audio
 
-            for buffer in input_file:
-                output_file.writeframes(buffer)
-
-    return wav_path
+    # 创建一个 BytesIO 对象来保存 WAV 文件
+    wav_io = BytesIO()
+    with wave.open(wav_io, "wb") as wav_file:
+        wav_file.setnchannels(mp3_audio.info.channels)
+        wav_file.setsampwidth(2)  # 16-bit samples
+        wav_file.setframerate(mp3_audio.info.sample_rate)
+        wav_file.writeframes(np.array(audio_data).tobytes())
+    wav_io.seek(0)
+    return wav_io
 
 def main():
     st.title("语音转文字转换器")
@@ -30,17 +32,11 @@ def main():
     uploaded_file = st.file_uploader("上传音频文件", type=["mp3"])
 
     if uploaded_file is not None:
-        mp3_path = "temp_audio_file.mp3"
-        
-        # 将上传的 MP3 文件保存到磁盘
-        with open(mp3_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-
-        # 将 MP3 文件转换为 WAV 文件
-        wav_file_path = convert_to_wav(mp3_path)
+        # 将上传的 MP3 文件转换为 WAV 文件
+        wav_file_io = convert_to_wav(uploaded_file.getvalue())
 
         # 识别 WAV 文件中的文本
-        text = transcribe_audio(wav_file_path)
+        text = transcribe_audio(wav_file_io)
 
         # 显示转换后的文本
         st.write("转录的文本:")
@@ -52,10 +48,6 @@ def main():
 
         # 提供下载链接
         st.download_button("下载转录文本文件", data=text, file_name='transcribed_text.txt', mime='text/plain')
-
-        # 清理临时文件
-        os.remove(mp3_path)
-        os.remove(wav_file_path)
 
 if __name__ == '__main__':
     main()
