140c83731851d3a15bd14b2518429f2652e83316
gaohazard <143723311+gaohazard@users.noreply.github.com>
2024-06-21 14:28:44 +0800

    Update utility_app3.py

--- utility_app3.py
+++ utility_app3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import speech_recognition as sr
-import subprocess
-import os
+from pydub import AudioSegment
 from io import BytesIO
 
 def transcribe_audio(audio_file):
@@ -11,17 +10,11 @@ def transcribe_audio(audio_file):
         text = recognizer.recognize_google(audio_data)
     return text
 
-def convert_to_wav(mp3_file):
-    wav_file = "temp_audio_file.wav"
-    command = ["ffmpeg", "-i", mp3_file, "-acodec", "pcm_s16le", "-ar", "16000", wav_file]
-    try:
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result.returncode != 0:
-            st.error(f"FFmpeg error: {result.stderr.decode('utf-8')}")
-    except FileNotFoundError:
-        st.error("FFmpeg未安装，请安装FFmpeg并确保其在系统路径中。")
-    except Exception as e:
-        st.error(f"转换过程中发生错误: {e}")
+def convert_to_wav(mp3_bytes):
+    audio = AudioSegment.from_file(BytesIO(mp3_bytes), format="mp3")
+    wav_file = BytesIO()
+    audio.export(wav_file, format="wav")
+    wav_file.seek(0)
     return wav_file
 
 def main():
@@ -30,30 +23,22 @@ def main():
     uploaded_file = st.file_uploader("上传音频文件", type=["mp3"])
 
     if uploaded_file is not None:
-        # 使用 BytesIO 来处理上传的文件
-        temp_mp3 = BytesIO(uploaded_file.getvalue())
-
-        # 将 BytesIO 对象写入临时文件
-        with open("temp_audio_file.mp3", "wb") as f:
-            f.write(temp_mp3.getbuffer())
-
-        # 转换 MP3 到 WAV
-        wav_file = convert_to_wav("temp_audio_file.mp3")
+        # 将上传的 MP3 文件转换为 WAV 文件
+        wav_file = convert_to_wav(uploaded_file.getvalue())
 
         # 识别 WAV 文件中的文本
-        if os.path.exists(wav_file):
-            text = transcribe_audio(wav_file)
+        text = transcribe_audio(wav_file)
 
-            # 显示转换后的文本
-            st.write("转录的文本:")
-            st.write(text)
+        # 显示转换后的文本
+        st.write("转录的文本:")
+        st.write(text)
 
-            # 将文本保存到文件中
-            with open('transcribed_text.txt', 'w', encoding='utf-8') as file:
-                file.write(text)
+        # 将文本保存到文件中
+        with open('transcribed_text.txt', 'w', encoding='utf-8') as file:
+            file.write(text)
 
-            # 提供下载链接
-            st.download_button("下载转录文本文件", data=text, file_name='transcribed_text.txt', mime='text/plain')
+        # 提供下载链接
+        st.download_button("下载转录文本文件", data=text, file_name='transcribed_text.txt', mime='text/plain')
 
 if __name__ == '__main__':
     main()
